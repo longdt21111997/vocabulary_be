ed6d80ac85e08c5851ce02c175ad8222184bb25f
longdt21111997 <63586598+longdt21111997@users.noreply.github.com>
2021-06-18 17:13:21 +0700

    remove auto index

--- src/main/java/tbl/tool/english/entities/VocabularyEntity.java
+++ src/main/java/tbl/tool/english/entities/VocabularyEntity.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
